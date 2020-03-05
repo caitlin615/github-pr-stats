7ca7c8f50993704c0db2702c26cc0a2b38cc34ec
Caitlin Elfring <celfring@renttherunway.com>
2020-03-04 20:25:50 -0500

    Use DEBUG for logging

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 
 # TODO: Query by date instead of by number of PRs
 NUM_PRS = os.getenv("NUM_PRS", 10)
+DEBUG = os.getenv("DEBUG", False)
 
 def main():
   repo_names = os.getenv("GITHUB_REPOS").split(",")
@@ -17,39 +18,43 @@ def main():
     print("--- ", repo.name)
     for pr in repo.get_pulls(state="all")[:NUM_PRS]:
       author = pr.user.login
-      print("#{} | merged: {} | state: {} | author: {} | title: {}".format(pr.number, pr.is_merged(), pr.state, pr.user.login, pr.title))
+      if DEBUG: print("#{} | merged: {} | state: {} | author: {} | title: {}".format(pr.number, pr.is_merged(), pr.state, pr.user.login, pr.title))
 
       for comment in pr.get_comments():
         # Don't count if the comments are by the author or if the commenter isn't on the team
         if comment.user.login == author or comment.user.login not in team_members: continue
         team_members[comment.user.login]["pr-comment"] += 1
-        print("    * pr-comment: {}: {}".format(comment.user.login, comment.body.splitlines()[0]))
+        if DEBUG: print("    * pr-comment: {}: {}".format(comment.user.login, comment.body.splitlines()[0]))
 
       for issue_comment in pr.get_issue_comments():
         # Don't count if the comments are by the author or if the commenter isn't on the team
         if issue_comment.user.login == author or issue_comment.user.login not in team_members: continue
         team_members[comment.user.login]["comment"] += 1
-        print("    * issue-comment: {}: {}".format(comment.user.login, comment.body.splitlines()[0]))
+
+        if DEBUG: print("    * issue-comment: {}: {}".format(comment.user.login, comment.body.splitlines()[0]))
 
       # This is just for logging
       for review in [review for review in pr.get_reviews() if review.state != "COMMENTED"]:
         if review.user.login not in team_members: continue
-        print("    * pr-review: {}: {}".format(review.user.login, review.state, review.body))
+        if DEBUG: print("    * pr-review: {}: {}".format(review.user.login, review.state, review.body))
 
       # Using `set` handles the uniqueness. You can approve a PR multiple times, but you should only get one point
       for approver in set([review.user.login for review in pr.get_reviews() if review.state != "COMMENTED"]):
         # Don't count if the reviewer isn't on the team
         if approver not in team_members: continue
         team_members[approver]["review"] += 1
-    print()
+
+    if DEBUG: print()
 
   # Calculate the totals
   for i in team_members:
     team_members[i]["total"] = team_members[i]["comment"] + team_members[i]["pr-comment"] + team_members[i]["review"]
 
-  print()
   for x in sorted(team_members.items(), key=lambda x: x[1]["total"], reverse=True):
-    print("{}: {}".format(x[0], x[1]))
+    if DEBUG:
+      print("{}: {}".format(x[0], x[1]))
+    else:
+      print("{}: {}".format(x[0], x[1]["total"]))
 
 if __name__== "__main__":
   main()
